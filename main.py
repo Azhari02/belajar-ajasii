74f16950f42048b0bde59a2a23bd9b89e0f08b57
Azhari02 <113221755+Azhari02@users.noreply.github.com>
2022-09-10 09:36:34 +0700

    commit pertama

---
+++ main.py
@@ -0,0 +1,28 @@
+azha="Nama saya azha"
+angka=12
+angka_pecahan=1.5
+tipe_boolean=True
+tipe_list=("bima, kiki, 13")
+tipe_set={1,2,3,3,3,4,4}
+pertambahan=343+331
+pengurangan=12-2
+perkalian=12*2
+a="nye"*17
+pembagian=17//7
+pembagian2=24/6
+modulus=17%6
+
+print(azha)
+print("tipe float", angka_pecahan)
+print("boolean :")
+print(tipe_boolean)
+print(tipe_list)
+print(tipe_list[2])
+print(tipe_set)
+print(pertambahan)
+print(pengurangan)
+print(perkalian)
+print(a)
+print(pembagian)
+print(pembagian2)
+print(modulus)
