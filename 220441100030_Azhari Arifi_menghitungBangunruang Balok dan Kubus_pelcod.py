f3ed588183f32b6f58de0b8643456b7159bef143
Azhari02 <113221755+Azhari02@users.noreply.github.com>
2022-09-16 16:51:38 +0700

    Add files via upload

---
+++ 220441100030_Azhari Arifi_menghitungBangunruang Balok dan Kubus_pelcod.py
@@ -0,0 +1,18 @@
+#Nama: Azhari Arifi
+#Nim : 220441100030
+
+#Balok
+print("Cara menghitung Luas Balok")
+p= int(input("Masukkan panjang balok"))
+l= int(input("Masukkan lebar balok"))
+t= int(input("Masukkan tinggi balok"))
+
+luaspermukaanbalok=(2*p*l)+(2*p*l)+(2*p*l)
+print("Hasil luas balok adalah:",luaspermukaanbalok)
+
+
+#Kubus
+print("Cara Menghitung luas Kubus")
+s=int(input("Masukkan panjang rusuk kubus"))
+luaskubus=6*s*s
+print("luas kubus diatas adalah",luaskubus)
